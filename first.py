4781b8dd0cf80566028ed49c43063c73a027d291
Natnael Getachew <natnaeln4dgmail.com>
2023-01-02 20:21:45 +0300

    adding first.py file

---
+++ first.py
@@ -0,0 +1,53 @@
+# print("hello world")
+# print("1+1")
+# anbes_age="anbes"
+# Nad_age="natty"
+# # total_age=anbes_age+Nad_age
+# print("hello G{Nad_age}")
+# print("hello{anbes_age}")
+# myvar=10
+# mylist=[1,2,3,myvar]
+# print(mylist)
+# employe={"rapper":"zeamanuel","producer":"Nad","productionmanager":"eyob"}
+# print(employe["rapper"])
+# print("updated news")
+# employe["rapper"]="N4d"
+# print(employe["rapper"])
+# print(employe)
+# book_price={"higher":[200,220,250],"lower":[100,120,150]}
+# # history=book_price("higher")
+# # print("first day price is :"{history[0]})
+# print(book_price.items())
+# mylist=[1,3,4,5,67,9]
+# for item in mylist:
+#     # if item==5:
+#           print("example a num: {item}")
+# employees={"ceo":"Nad","manager":"G"}
+# for postion in employees:
+#         print(postion  employees[postion])
+# def name_of_function(name):
+#        print("hello "+name)
+#         #  print("hello")
+# def alem(name):
+#        e=["a","n","d","d"]
+#        studentInfo={"name":"Natnael getachew","city":"jigjiga","age":21,"department":"software"}
+#        for item in range(5):
+
+#         print("hello {name}")
+
+#        for u in e:
+
+#         print(u) 
+#        for info in studentInfo:
+
+#         print(info); 
+name=input("enter ur name")
+print(name)
+# def nameInput():
+       
+#         # alem(name)
+
+# nameInput()
+
+# name_of_function("Nad")
+
